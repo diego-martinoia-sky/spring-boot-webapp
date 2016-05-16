74b7c813317aef68da6e02057f78b8361e413d94
Diego Martinoia DMA77 <diego.martinoia@sky.uk>
2016-05-16 12:54:20 +0100

    test should succeed

--- src/test/java/com/sky/interactive/ApplicationTest.java
+++ src/test/java/com/sky/interactive/ApplicationTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 
 public class ApplicationTest {
     @Test
-    public void failingTest() {
-        assertTrue(false);
+    public void successTest() {
+        assertTrue(true);
     }
 }
