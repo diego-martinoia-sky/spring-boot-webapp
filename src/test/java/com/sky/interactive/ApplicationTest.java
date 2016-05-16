90324934b10bc8a5f7a479d83430ee4eb7f7f6e7
Diego Martinoia DMA77 <diego.martinoia@sky.uk>
2016-05-16 12:52:06 +0100

    test should fail

---
+++ src/test/java/com/sky/interactive/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.sky.interactive;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+    @Test
+    public void failingTest() {
+        assertTrue(false);
+    }
+}
