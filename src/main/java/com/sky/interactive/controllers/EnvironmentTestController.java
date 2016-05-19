a80629122a7c50be92024e28b8908bc07de70c3c
Diego Martinoia DMA77 <diego.martinoia@sky.uk>
2016-05-19 14:20:41 +0100

    scrutinytest

--- src/main/java/com/sky/interactive/controllers/EnvironmentTestController.java
+++ src/main/java/com/sky/interactive/controllers/EnvironmentTestController.java
@@ -8,12 +8,14 @@ public class EnvironmentTestController {
 
     static final String ENVIRONMENT_CONF = "FILE_ENVIRONMENT_CONF";
     static final String LOG4J_CONF = "FILE_LOG4J_CONFIGURATION";
+    static final String PATH = "PATH";
 
     @RequestMapping("/")
     public String index() {
         String output = "";
         output+=ENVIRONMENT_CONF+"="+System.getenv(ENVIRONMENT_CONF)+"\n";
-        output+=LOG4J_CONF+"="+System.getenv(LOG4J_CONF);
+        output+=LOG4J_CONF+"="+System.getenv(LOG4J_CONF)+"\n";
+        output+=PATH+"="+System.getenv(PATH);
         return output;
     }
 }
