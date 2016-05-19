49bcd5c74514eef718996c46f655f0d57838c9d6
Diego Martinoia DMA77 <diego.martinoia@sky.uk>
2016-05-19 16:26:36 +0100

    scrutinytest

--- src/main/java/com/sky/interactive/controllers/EnvironmentTestController.java
+++ src/main/java/com/sky/interactive/controllers/EnvironmentTestController.java
@@ -8,7 +8,6 @@ public class EnvironmentTestController {
 
     static final String ENVIRONMENT_CONF = "file.environment.conf";
     static final String LOG4J_CONF = "file.log4j.configuration";
-    static final String PATH = "PATH";
 
     @RequestMapping("/")
     public String index() {
@@ -16,18 +15,6 @@ public class EnvironmentTestController {
 
         output+=ENVIRONMENT_CONF+"="+System.getProperty(ENVIRONMENT_CONF)+"\n\n";
         output+=LOG4J_CONF+"="+System.getProperty(LOG4J_CONF);
-
-        output+="ENV:\n";
-        for(String s: System.getenv().keySet()) {
-            output+=s+" = "+System.getenv(s)+"\n";
-        }
-
-        output+="\n\n\n";
-
-        output+="SYS:\n";
-        for(String s: System.getProperties().stringPropertyNames()) {
-            output+=s+" = "+System.getProperty(s)+"\n";
-        }
 
         return output;
     }
